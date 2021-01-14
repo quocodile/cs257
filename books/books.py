44e48e8e294fcbb595edce260e5c1bbb3879be21
Quoc Nguyen <nguyenq2@carleton.edu>
2021-01-14 17:56:52 -0600

    finishing, working on output

--- books/books.py
+++ books/books.py
@@ -3,30 +3,45 @@ import csv
 
 def get_command():
     parser = argparse.ArgumentParser(description="This allows you to query from a books database")
-    parser.add_argument('commands', nargs='+')
+    parser.add_argument('query_type', choices=['title', 'author', 'years'], nargs='?', default='-h')
+    parser.add_argument('query_arguments', nargs='+')
     args = parser.parse_args()
-    return args.commands
+    return args
 
-def search(commands):
+def search():
+    query_type = get_command().query_type
+    query_arguments = get_command().query_arguments
     with open('books.csv', newline='') as f:
         reader = csv.reader(f)
-        if (commands[0] == 'title'):
+        if (query_type == 'title'):
             for row in reader: 
-                if (row[0] == commands[1]):
-                    print(row)
+                if (row[0].lower() in query_arguments[0].lower()):
+                    print("Titles containing '" +  query_arguments[0].lower() + "':", row[0])
             return
-        if (commands[0] == 'author'):
-            print(commands[1] + " " + commands[2])
+        if (query_type == 'author'):
+            authors_books = []
             for row in reader:
-                print(row[2]) 
-                if (commands[1] + " " + commands[2]  == row[2]):
-                    print(row)
+                if query_arguments[0].lower() in row[2].lower():
+                    if row[2].lower() not in authors_books:
+                        authors_books.append({'author': row[2].lower(), 'books':[row[1].lower()]})
+                    else:
+                        authors_books.append([row[2].lower() => [row[1].lower()]])
             return
-        if (commands[0] == 'years'):
-            print(commands[1], commands[2])
+        if (query_type == 'years'):
+            for row in reader:
+                if len(query_arguments) == 2:
+                    if row[1] >= query_arguments[0] and row[1] <= query_arguments[1]:
+                        print(row)
+                elif len(query_arguments) == 1:
+                    if row[1] >= query_arguments[0]:
+                        print(row)
+                elif len(query_arguments) > 2:
+                    print("yeet")
+                else:
+                    print(row)
         
 def main():
-    search(get_command())
+    search()
 
 if __name__ == '__main__':
     main()
