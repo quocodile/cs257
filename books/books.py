503a8e9583023af8407b8dcfbef2ab365d3e032e
Quoc Nguyen <nguyenq2@carleton.edu>
2021-01-14 22:11:42 -0600

    finished books.py

--- books/books.py
+++ books/books.py
@@ -1,24 +1,26 @@
 import argparse
 import csv
-import sys
 
-def get_command():
+def create_parser():
     parser = argparse.ArgumentParser(description="This allows you to query from a books database")
-    parser.add_argument('query_type', choices=['title', 'author', 'years'], nargs='?', default='-h', help='User must choose a query type')
+    parser.add_argument('query_type', choices=['title', 'author', 'years'], nargs=1, help='User must choose a query type')
     parser.add_argument('query_arguments', nargs='+', help='Values provided as argument for query type')
+    return parser
+
+def get_command(parser):
     args = parser.parse_args()
     return args
 
 def search():
-    query_type = get_command().query_type
-    query_arguments = get_command().query_arguments
+    query_type = get_command(create_parser()).query_type[0]
+    query_arguments = get_command(create_parser()).query_arguments
     with open('books.csv', newline='') as f:
         reader = csv.reader(f)
         if (query_type == 'title'):
+            print("Titles containing '" +  query_arguments[0].lower() + "':")
             for row in reader: 
-                if (row[0].lower() in query_arguments[0].lower()):
-                    print("Titles containing '" +  query_arguments[0].lower() + "':", row[0])
-            return
+                if (query_arguments[0].lower() in row[0].lower()):
+                    print(" * " + row[0])
         if (query_type == 'author'):
             authors_books = {}
             for row in reader:
@@ -29,7 +31,6 @@ def search():
                         authors_books[row[2]].append(row[0])
             for key in authors_books.keys():
                 print('Author: ' + key + ' Books: ' + ', '.join(authors_books[key]))
-            return
         if (query_type == 'years'):
             if len(query_arguments) == 2:
                 print('Books published between ' + query_arguments[0] + ' and ' + query_arguments[1] + ': ')
@@ -42,7 +43,7 @@ def search():
                     if row[1] >= query_arguments[0]:
                         print(" * " + row[0])
             else:
-                get_command().print_help()
+                create_parser().print_help()
 def main():
     search()
 
