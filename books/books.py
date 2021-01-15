50019d25835d960cb4a860a1c53cc99bca93c6a2
Ignacio Rodriguez <rodriguezn@carleton.edu>
2021-01-14 21:20:13 -0600

    finalized output

--- books/books.py
+++ books/books.py
@@ -1,10 +1,11 @@
 import argparse
 import csv
+import sys
 
 def get_command():
     parser = argparse.ArgumentParser(description="This allows you to query from a books database")
-    parser.add_argument('query_type', choices=['title', 'author', 'years'], nargs='?', default='-h')
-    parser.add_argument('query_arguments', nargs='+')
+    parser.add_argument('query_type', choices=['title', 'author', 'years'], nargs='?', default='-h', help='User must choose a query type')
+    parser.add_argument('query_arguments', nargs='+', help='Values provided as argument for query type')
     args = parser.parse_args()
     return args
 
@@ -19,27 +20,29 @@ def search():
                     print("Titles containing '" +  query_arguments[0].lower() + "':", row[0])
             return
         if (query_type == 'author'):
-            authors_books = []
+            authors_books = {}
             for row in reader:
                 if query_arguments[0].lower() in row[2].lower():
-                    if row[2].lower() not in authors_books:
-                        authors_books.append({'author': row[2].lower(), 'books':[row[1].lower()]})
+                    if row[2] not in authors_books.keys():
+                        authors_books[row[2]] = [row[0]]
                     else:
-                        authors_books.append([row[2].lower() => [row[1].lower()]])
+                        authors_books[row[2]].append(row[0])
+            for key in authors_books.keys():
+                print('Author: ' + key + ' Books: ' + ', '.join(authors_books[key]))
             return
         if (query_type == 'years'):
-            for row in reader:
-                if len(query_arguments) == 2:
+            if len(query_arguments) == 2:
+                print('Books published between ' + query_arguments[0] + ' and ' + query_arguments[1] + ': ')
+                for row in reader:
                     if row[1] >= query_arguments[0] and row[1] <= query_arguments[1]:
-                        print(row)
-                elif len(query_arguments) == 1:
+                        print(" * " + row[0])
+            elif len(query_arguments) == 1:
+                print('Books published after ' + query_arguments[0] + ': ')
+                for row in reader:
                     if row[1] >= query_arguments[0]:
-                        print(row)
-                elif len(query_arguments) > 2:
-                    print("yeet")
-                else:
-                    print(row)
-        
+                        print(" * " + row[0])
+            else:
+                get_command().print_help()
 def main():
     search()
 
