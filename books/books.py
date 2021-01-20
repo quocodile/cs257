d3194022b5327ba7962d30ae65d36298a8660a1d
Ignacio Rodriguez <rodriguezn@carleton.edu>
2021-01-19 21:51:50 -0600

    Added most of the revisions from Peer Code Review

--- books/books.py
+++ books/books.py
@@ -1,54 +1,37 @@
-import argparse
-import csv
-
 '''
 Authors: Nacho Rodriguez-Cortes, Quoc Nguyen
 Description: program perfoms search queries on a books.csv file.
 '''
 
+import csv
+import argparse
+
 def create_parser():
     parser = argparse.ArgumentParser(description="This allows you to query from a books database")
-    parser.add_argument('query_type', choices=['title', 'author', 'years'], nargs=1, help='specify only one query type at a time')
     parser.add_argument('query_arguments', nargs='+', metavar=('args'), help='provide arguments for query')
     return parser
 
-'''
-gets all arguments provided through the command line
-'''
 def get_command_arguments(parser):
+    '''
+    gets all arguments provided through the command line
+    '''
     args = parser.parse_args()
     return args
 
-'''
-gets query type (by title, by author, or by publication year) the user had chosen
-'''
-def get_query_type():
-    query_type = get_command_arguments(create_parser()).query_type[0]
-    return query_type
-
-'''
-gets the arguments user provided as necessitated by the query type they chose
-'''
 def get_query_arguments():
+    '''
+    gets the arguments user provided as necessitated by the query type they chose
+    '''
     query_arguments = get_command_arguments(create_parser()).query_arguments
     return query_arguments
 
-def query_handler(query_type, query_arguments):
-    with open('books.csv', newline='') as f:
-        reader = csv.reader(f)
-        if (query_type == 'title'):
-            query_by_title(query_arguments[0], reader)
-        if (query_type == 'author'):
-            query_by_author(query_arguments[0], reader)
-        if (query_type == 'years'):
-            query_by_years(query_arguments, reader)
-
 def query_by_title(search_string, csv_reader):
-    print("Titles containing '" +  search_string.lower() + "':")
+    books = []
     for row in csv_reader:
        title = row[0]
        if (search_string.lower() in title.lower()):
-          print_book(title)
+          books.append(title)
+    return books
     
 def query_by_author(search_string, csv_reader):
     authors_books = {} #a dictionary with author names as keys and a list of books as values
@@ -59,28 +42,61 @@ def query_by_author(search_string, csv_reader):
                 authors_books[author] = [row[0]]
             else:
                 authors_books[row[2]].append(row[0])
-    for key in authors_books.keys():
-        print('Author: ' + key + ' Books: ' + ', '.join(authors_books[key]))
+    return authors_books
   
 def query_by_years(query_arguments, csv_reader):
-    if len(query_arguments) == 2:
-        print('Books published between ' + query_arguments[0] + ' and ' + query_arguments[1] + ': ')
+    books = []
+    if len(query_arguments) == 3:
+        start_year = query_arguments[1]
+        end_year = query_arguments[2]
         for row in csv_reader:
-            if row[1] >= query_arguments[0] and row[1] <= query_arguments[1]:
-                print(" * " + row[0])
-    elif len(query_arguments) == 1:
-        print('Books published after ' + query_arguments[0] + ': ')
+            title = row[0]
+            publication_year = row[1]
+            if publication_year >= start_year and publication_year <= end_year:
+                books.append(title)
+        return books
+    elif len(query_arguments) == 2:
+        start_year = query_arguments[1]
         for row in csv_reader:
-            if row[1] >= query_arguments[0]:
-                print_book(row[0])
+            title = row[0]
+            publication_year = row[1]
+            if publication_year >= start_year:
+                books.append(title)
+        return books
     else:
         create_parser().print_help()
 
-def print_book(title):
-    print(" * " + title)
+def print_books(books_array):
+    for book in books_array:
+        print(" * " + book)
 
 def main():
-    query_handler(get_query_type(), get_query_arguments())
+    query_type = get_query_arguments()[0] 
+    title = get_query_arguments()[1]
+ 
+    with open('books.csv', newline='') as f:
+        reader = csv.reader(f)
+        if (query_type == 'title'):
+            title = get_query_arguments()[1]
+            print("All books with the title containing", title)
+            books = query_by_title(title, reader)
+            print_books(books)
+        if (query_type == 'author'):
+            author = get_query_arguments()[1]
+            query_by_author(author, reader)
+        if (query_type == 'years'):
+            query_arguments = get_query_arguments()
+            books = query_by_years(query_arguments, reader)
+            if len(query_arguments) == 3:
+                start_year = query_arguments[1]
+                end_year = query_arguments[2]
+                print("Titles published between ", start_year, " and ", end_year)
+                print_books(books)
+            elif len(query_arguments) == 2:
+                start_year = query_arguments[1]
+                print("Title published after ", start_year)
+                print_books(books) 
+
 
 if __name__ == '__main__':
     main()
