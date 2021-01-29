953213eb9a4b351d91baaf9e6e785f917dedfbc2
Quoc Nguyen <nguyenq2@carleton.edu>
2021-01-29 16:41:41 -0600

    put everything in database folder

--- database/archive/queries.sql
+++
@@ -1,7 +0,0 @@
-SELECT noc FROM regions ORDER BY noc;
-
-SELECT DISTINCT name FROM athletes WHERE team='Kenya';
- 
-SELECT * FROM medals WHERE athlete_name LIKE '%Louganis%' ORDER BY year ASC;
-
-SELECT COUNT(medal), noc FROM medals WHERE medal = 'Gold' GROUP BY noc ORDER BY COUNT(medal) DESC;
---
+++ database/config.py
@@ -0,0 +1,3 @@
+database = 'olympics'
+user = 'quocodile'
+password = '_comPscikiD_320'
