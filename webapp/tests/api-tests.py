b642f8b4db558f419fa865b95a85d9fc1df45db2
James Marlin <marlinj@carleton.edu>
2021-02-18 23:25:33 -0600

    added code

--- webapp/tests/api-tests.py
+++ webapp/tests/api-tests.py
@@ -21,8 +21,23 @@ class ServerTester(unittest.TestCase):
   def test_get_nonexistent_anime(self):
      self.assertFalse(self.server.get_anime('Humpty Dumpty'))
 
-  def test_get_existing_anime(self):
+  def test_get_animes_in_genre(self):
     anime1_dictionary = {'name' : 'Dragon Ball Z', 'episodes' : 291, 'genre' : 'Action, Shonen', 'MAL_rating' : 8.16, 'our_rating' : 0}
-    anime1_dictionary
-    self.assertEqual(self.server.get_anime('Dragon Ball Z'), anime_dictionary)
+    anime2_dictionary = {'name' : 'Naruto', 'episodes' : 220, 'genre' : 'Action, Shonen', 'MAL_rating' : 7.91, 'our_rating' : 0}
+    action_list = [anime1_dictionary,anime2_dictionary]
+    self.assertEqual(self.server.get_animes_in_genre('Action'), action_list)
+
+  def test_get_user_watchlist(self):
+    anime_dictionary1 = {'name' : 'Dragon Ball Z', 'episodes' : 291, 'genre' : 'Action, Shonen', 'MAL_rating' : 8.16, 'our_rating' : 0}
+    anime_dictionary2 = {'name' : 'Naruto', 'episodes' : 220, 'genre' : 'Action, Shonen', 'MAL_rating' : 7.91, 'our_rating' : 0}
+    anime_dictionary3 = {'name' : 'Bleach', 'episodes' : 366, 'genre' : 'Action, Shonen', 'MAL_rating' : 7.80, 'our_rating' : 0}
+    watchlist = [anime_dictionary1, anime_dictionary2, anime_dictionary3]
+    self.assertEqual(self.server.get_user_watchlist(user_id), anime_dictionary2)
+
+  def test_create_user_watchlist(self):
+    self.assertTrue(self.server.create_user_watchlist(user_id,anime_id))
+
+  def test_get_recommendations(self):
+    self.assertTrue(self.server.get_recommendations(user_id,genre))
   
+
