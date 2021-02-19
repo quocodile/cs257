d956cb18a55354c85c1f3a2c016bf09bcf45970e
Quoc Nguyen <nguyenq2@carleton.edu>
2021-02-18 20:42:45 -0600

    created the initial drafts of necesarry assignments due 2/18

---
+++ webapp/tests/api-tests.py
@@ -0,0 +1,13 @@
+'''
+  Kevin Chen, James Marlin, Quoc Nguyen
+
+  this program is used to implement unit
+  tests for our web application as part
+  of our final project
+'''
+
+import unittest
+class Server(unittest.TestCase):
+  def setUp(self):
+
+  def tearDown(self):
