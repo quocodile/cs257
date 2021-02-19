b41eac64bbe62d5ad730a35030bbd91009298cde
Quoc Nguyen <nguyenq2@carleton.edu>
2021-02-18 22:45:43 -0600

    unit testing code

--- webapp/tests/api-tests.py
+++ webapp/tests/api-tests.py
@@ -5,9 +5,24 @@
   tests for our web application as part
   of our final project
 '''
-
+import server
 import unittest
-class Server(unittest.TestCase):
+class ServerTester(unittest.TestCase):
   def setUp(self):
+    self.server.Server() 
 
   def tearDown(self):
+    pass
+
+  def test_get_existing_anime(self):
+    anime_dictionary = {'name' : 'Dragon Ball Z', 'episodes' : 291, 'genre' : 'Action, Shonen', 'MAL_rating' : 8.16, 'our_rating' : 0}
+    self.assertEqual(self.server.get_anime('Dragon Ball Z'), anime_dictionary)
+
+  def test_get_nonexistent_anime(self):
+     self.assertFalse(self.server.get_anime('Humpty Dumpty'))
+
+  def test_get_existing_anime(self):
+    anime1_dictionary = {'name' : 'Dragon Ball Z', 'episodes' : 291, 'genre' : 'Action, Shonen', 'MAL_rating' : 8.16, 'our_rating' : 0}
+    anime1_dictionary
+    self.assertEqual(self.server.get_anime('Dragon Ball Z'), anime_dictionary)
+  
