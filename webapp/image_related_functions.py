fc9f64f4359d7a6316abc6e7e24e06911e33948d
Quoc Nguyen <nguyenq2@carleton.edu>
2021-02-23 11:47:10 -0600

    some print statements in image_related_functions.py

--- webapp/image_related_functions.py
+++ webapp/image_related_functions.py
@@ -6,7 +6,8 @@ def get_animes_imagepaths():
   the names of animes and values are
   the paths to their pictures. 
   '''
-  print('putting images onto server, please wait just a few brief moments.')
+  print("Putting image paths onto server, please wait just a few brief moments.")
+  print("After debugger is active, site will be fully ready to go.")
   anime_imagespaths_dictionary = {}
   for root, dirs, files in os.walk('static/images/'):
     for name in dirs:
